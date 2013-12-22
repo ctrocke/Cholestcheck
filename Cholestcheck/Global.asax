dd0fc2ead62a3976d4cf1f0ba7b0379cfbac24e7
ctrocke <ctrocke@gmail.com>
2013-12-22 14:37:14 -0500

    initialcommit

---
+++ Cholestcheck/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cholestcheck.Global" Language="C#" %>
---
+++ Cholestcheck/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cholestcheck.Startup))]
+namespace Cholestcheck
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
